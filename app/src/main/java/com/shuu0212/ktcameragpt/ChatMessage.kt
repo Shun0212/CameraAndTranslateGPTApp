34672c70a684770c84b05edab4cfb8d8c4aea253
Shuu <136542198+Shun0212@users.noreply.github.com>
2024-10-12 13:05:39 +0900

    Delete app directory

--- app/src/main/java/com/shuu0212/ktcameragpt/ChatMessage.kt
+++
@@ -1,6 +0,0 @@
-package com.shuu0212.ktcameragpt
-
-data class ChatMessage(
-    val message: String,
-    val isUser: Boolean
-)
--- app/src/main/java/com/shuu0212/ktcameragpt/News.kt
+++
@@ -1,5 +0,0 @@
-package com.shuu0212.ktcameragpt
-
-
-
-data class News(val title: String, val description: String, val url: String, val imageUrl: String)
